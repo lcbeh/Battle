c4c164a30908a8ade80d0a883d01dfe0d800eca6
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-10-11 16:52:46 +0100

    added functionality to enter player names

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb(:index)
   end
+
+  post '/names' do
+  	p params
+  	@player1 = params[:player1]
+  	@player2 = params[:player2]
+  	erb(:play)
+  end 
 
   # start the server if ruby file executed directly
   run! if app_file == $0
